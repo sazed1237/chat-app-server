13674a1e0ed055a20baa949b2b6da9cbc6812db1
sazed1237 <sazedulislam9126@gmail.com>
2024-09-10 02:30:06 +0600

    add cors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser')
 const connectDB = require('./config/connectDB');
 const router = require('./routes/Router');
-const {app, server} = require('./socket/socket')
+const { app, server } = require('./socket/socket')
 
 
 // const app = express()
@@ -14,7 +14,8 @@ const port = process.env.PORT || 5000
 
 // middleware
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [process.env.FRONTEND_URL_PRODUCTION || "http://localhost:5173"],
+    methods: ["GET", "POST"],
     credentials: true
 }))
 app.use(express.json())
--- socket/socket.js
+++ socket/socket.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: [process.env.FRONTEND_URL_PRODUCTION || "http://localhost:5173"], 
         methods: ["GET", "POST"],
         credentials: true
     }
